298dbefc1c13a9d946ee060c1a50fc026c6b304b
Shashiawari <109402358+Shashiawari@users.noreply.github.com>
2024-07-26 21:54:38 +0530

    comit

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Navbar.css";
 import NavLink from "./list";
+
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-md ">
+      <nav className="navbar navbar-expand-md">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img
               src="https://res.cloudinary.com/dq7brjjnz/image/upload/v1698510114/Picture1_xekuhi.png"
               height={"50px"}
+              alt="Brand"
             ></img>
           </a>
           <button
@@ -21,27 +23,27 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-          <input type="checkbox" id="checkbox"/>
-          <label htmlFor="checkbox" className="toggle">
+            <input type="checkbox" id="checkbox" />
+            <label htmlFor="checkbox" className="toggle">
               <div className="bars" id="bar1"></div>
               <div className="bars" id="bar2"></div>
               <div className="bars" id="bar3"></div>
-          </label>
+            </label>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto ">
-                <li className="nav-item">
-                  <NavLink href={"/"} children="home" />
-                </li>
-                <li className="nav-item">
-                  <NavLink href={"/about"} children="about" />
-                </li>
-                <li className="nav-item">
-                  <NavLink href={"/projects"} children="projects" />
-                </li>
-                <li className="nav-item">
-                  <NavLink href={"/contact"} children="contact" />
-                </li>
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink href={"/"}>home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink href={"/about"}>about</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink href={"/projects"}>projects</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink href={"/contact"}>contact</NavLink>
+              </li>
             </ul>
           </div>
         </div>
