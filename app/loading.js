ce8c39f688d71b21bae9472075888b2b43ac4a4a
Shashiawari <109402358+Shashiawari@users.noreply.github.com>
2024-07-26 22:12:52 +0530

    ss

---
+++ app/loading.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='lsl'>
+    <div className="loader">
+    <div className="cube">
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default loading
